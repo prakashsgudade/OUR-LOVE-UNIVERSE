5092835994d5d51754dfd81062f7c9187d2205c2
prakashsgudade <prakashsgudade@gmail.com>
2026-02-05 00:15:27 +0530

    Update main-logic.js

--- js/main-logic.js
+++ js/main-logic.js
@@ -1,1 +1,8 @@
+function enterKingdom() {
+    // Music play karo
+    const music = document.getElementById('bgMusic');
+    if(music) music.play();
 
+    // Day 1 par le jao
+    window.location.href = "chapters/day1.html";
+}
