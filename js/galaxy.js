0bc790e94ddcf24424b560f602c7e6e73d7860f6
prakashsgudade <prakashsgudade@gmail.com>
2026-02-08 10:24:42 +0530

    Create galaxy.js

---
+++ js/galaxy.js
@@ -0,0 +1,34 @@
+const canvas = document.getElementById("galaxy");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars=[];
+
+for(let i=0;i<120;i++){
+    stars.push({
+        x:Math.random()*canvas.width,
+        y:Math.random()*canvas.height,
+        r:Math.random()*2
+    });
+}
+
+function animate(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    ctx.fillStyle="white";
+
+    stars.forEach(s=>{
+        ctx.beginPath();
+        ctx.arc(s.x,s.y,s.r,0,Math.PI*2);
+        ctx.fill();
+
+        s.y+=0.2;
+        if(s.y>canvas.height) s.y=0;
+    });
+
+    requestAnimationFrame(animate);
+}
+
+animate();
