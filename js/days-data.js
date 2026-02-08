7590b13ff287bcc4d0f6da680b5b0b1bb866ec43
prakashsgudade <prakashsgudade@gmail.com>
2026-02-08 16:26:00 +0530

    Create days-data.js

---
+++ js/days-data.js
@@ -0,0 +1,21 @@
+const DAYS={
+
+1:{
+title:"First Spark",
+text:"Yahi se sab shuru hua",
+img:"assets/images/photo1.jpg"
+},
+
+2:{
+title:"Attachment",
+text:"Dil aadat ban gaya",
+img:"assets/images/photo2.jpg"
+},
+
+3:{
+title:"Deep Bond",
+text:"Ab tum sirf tum nahi ho",
+img:"assets/images/photo3.jpg"
+}
+
+}
