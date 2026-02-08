265465c60b959c54f424d970b92f685f5006fdb0
prakashsgudade <prakashsgudade@gmail.com>
2026-02-08 12:07:05 +0530

    Update day-engine.js

--- js/day-engine.js
+++ js/day-engine.js
@@ -1,39 +1,21 @@
-const TOTAL_DAYS = 5000;
-
-const THEMES = [
-"linear-gradient(135deg,#2b1055,#7597de)",
-"linear-gradient(135deg,#000428,#004e92)",
-"linear-gradient(135deg,#200122,#6f0000)",
-"linear-gradient(135deg,#0f2027,#2c5364)",
-"linear-gradient(135deg,#1f4037,#99f2c8)",
-"linear-gradient(135deg,#42275a,#734b6d)"
-];
-
 const grid = document.getElementById("daysGrid");
 
-for(let i=1;i<=TOTAL_DAYS;i++){
+const TOTAL = 5000; // infinite feel
+
+for(let i=1;i<=TOTAL;i++){
 
 let card=document.createElement("div");
 card.className="card";
 
-card.style.background =
-THEMES[i % THEMES.length];
+card.style.background=
+`linear-gradient(${i*37%360}deg,#111,#222)`;
 
 card.innerHTML="Day "+i;
 
 card.onclick=()=>{
-openDay(i);
+localStorage.setItem("currentDay",i);
+window.location.href="day.html";
 };
 
 grid.appendChild(card);
-
-}
-
-// OPEN DAY PAGE
-function openDay(day){
-
-localStorage.setItem("currentDay",day);
-
-window.location.href="day.html";
-
 }
