c131142787e8d9e75e39601b7c1ad885c7527463
prakashsgudade <prakashsgudade@gmail.com>
2026-02-08 10:28:54 +0530

    Update day-engine.js

--- js/day-engine.js
+++ js/day-engine.js
@@ -1,24 +1,52 @@
-// URL se day number pakdo
-const params = new URLSearchParams(window.location.search);
-const day = params.get("day");
+const params=new URLSearchParams(window.location.search);
+const day=params.get("day");
 
-const data = DAYS[day];
+const data=DAYS[day];
 
-if(!data){
-    document.body.innerHTML="Day Not Found";
+// Emotional progression theme
+function applyEmotion(e){
+if(e==1)document.body.style.background="#000";
+if(e==2)document.body.style.background="#200";
+if(e==3)document.body.style.background="#002";
 }
 
-// Title change
-document.title = "Day " + day;
+applyEmotion(data.emotion);
 
-// Page render
-document.getElementById("dayContainer").innerHTML = `
-    <h1>Day ${day}</h1>
-    <h2>${data.title}</h2>
 
-    <img src="${data.image}" width="300">
+// Unlock cinematic
+document.getElementById("rose").onclick=()=>{
+document.getElementById("unlock").style.display="none";
+document.getElementById("content").style.display="block";
+render();
+};
 
-    <p>${data.text}</p>
 
-    <audio controls src="${data.music}"></audio>
+// Render Layout auto change
+function render(){
+
+let layout="";
+
+if(day%2==0){
+layout=`
+<div class="card">
+<div class="inner">
+<div class="front">
+<h2>${data.title}</h2>
+</div>
+<div class="back">
+<img src="${data.img}" width="100%">
+<p>${data.text}</p>
+</div>
+</div>
+</div>
 `;
+}else{
+layout=`
+<h1>${data.title}</h1>
+<img src="${data.img}" width="300">
+<p>${data.text}</p>
+`;
+}
+
+document.getElementById("content").innerHTML=layout;
+}
