2d9dc8829b356829ca59ee4f9db4052674ad99a0
prakashsgudade <prakashsgudade@gmail.com>
2026-02-08 10:01:42 +0530

    Create days-data.js

---
+++ data/days-data.js
@@ -0,0 +1,27 @@
+const DAYS = {
+
+1:{
+    theme:"gold",
+    title:"The Beginning",
+    image:"assets/images/photo1.jpg",
+    music:"assets/audio/song1.mp3",
+    text:"Yahan se kahani shuru hui..."
+},
+
+2:{
+    theme:"rose",
+    title:"First Smile",
+    image:"assets/images/photo2.jpg",
+    music:"assets/audio/song2.mp3",
+    text:"Tumhari muskan..."
+},
+
+3:{
+    theme:"dark",
+    title:"Secret Memory",
+    image:"assets/images/photo3.jpg",
+    music:"assets/audio/song3.mp3",
+    text:"Ek pal jo sirf hamara tha..."
+}
+
+}
