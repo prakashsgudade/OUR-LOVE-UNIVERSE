6a0fd8ab7a7f2d3a6b2459d030fcfe9428ebc797
prakashsgudade <prakashsgudade@gmail.com>
2026-02-08 16:57:45 +0530

    Update day-engine.js

--- js/day-engine.js
+++ js/day-engine.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     const params = new URLSearchParams(window.location.search);
-    const dayId = params.get('d'); 
-    const data = loveDays[dayId];
+    const dayNumber = params.get('d');
+    const data = loveDays[dayNumber];
 
     if (data) {
         document.getElementById('day-title').innerText = data.title;
@@ -9,11 +9,16 @@ window.onload = function() {
         document.getElementById('day-message').innerText = data.message;
         document.getElementById('day-img').src = data.image;
         
+        // Music logic
         if(data.song) document.getElementById('bg-music').src = data.song;
-        if(data.voice) document.getElementById('voice-note').src = data.voice;
-        else document.getElementById('voice-section').style.display = "none";
         
+        // Voice note logic
+        if(data.voice) {
+            document.getElementById('voice-note').src = data.voice;
+        } else {
+            document.getElementById('voice-section').style.display = "none";
+        }
     } else {
-        document.body.innerHTML = "<div class='error'>muskan wo din bahot jaldi ayega ,! ❤️</div>";
+        document.body.innerHTML = "<h2 style='color:white; text-align:center; margin-top:50px;'>Bhai, ye memory abhi upload nahi hui! ❤️</h2>";
     }
-}
+};
